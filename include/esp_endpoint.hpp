eb98d116f5a5a97866cb88903357ca1cb6caa62b
Jannis Baum <jannis.baum@gmail.com>
2021-11-29 17:52:19 +0100

    connect to wifi

--- include/esp_endpoint.hpp
+++ include/esp_endpoint.hpp
@@ -1,13 +1,27 @@
 #ifndef ESP_ENDPOINT
 #define ESP_ENDPOINT
 
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 
 class ESPEndpoint {
+public:
+    // struct Value {
+    //     String name;
+    //     int value;
+    //     int (*callback)(int);
+    // };
+
 private:
+    WiFiServer server;
+    String name;
+    // Value* values; int count;
+    // int extractValueFor(String name, String source);
 
 public:
-
+    ESPEndpoint(const char* ssid, const char* password, const char* name);//, Value* values, int count);
+    void iterate();
 };
 
 #endif
+
--- src/esp_endpoint.cpp
+++ src/esp_endpoint.cpp
@@ -1,1 +1,26 @@
 #include "esp_endpoint.hpp"
+
+ESPEndpoint::ESPEndpoint(const char* ssid, const char* password, const char* name)
+: server(80), name(name) {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) delay(3);
+    this->server.begin();
+
+    Serial.println(WiFi.localIP());
+}
+
+void ESPEndpoint::iterate() {
+    WiFiClient client = this->server.available();
+    if (!client) return;
+
+    while(!client.available()) delay(3);
+
+    String request = client.readStringUntil('\r');
+
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type: text/json\n");
+    client.println("{");
+    client.print("    \"name\": \""); client.print(this->name); client.println("\"");
+    client.println("}");
+}
+
